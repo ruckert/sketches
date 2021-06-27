f8dff65977240d3faf2d2fd5ec1966ecfefad53c
ruckert <augustodesign@gmail.com>
2021-06-27 16:54:18 -0300

    basic canvas adjustments and something like a first sketch

---
+++ sketches/001/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(displayWidth, displayHeight)
+    background("blue")
+}
+
+function draw() {
+    fill("red")
+    rect(40,40,400,444)
+}
--- sketches/_basic_canvas/sketch.js
+++ sketches/_basic_canvas/sketch.js
@@ -1,7 +1,8 @@
 function setup() {
-  // put setup code here
+    // createCanvas(displayWidth, displayHeight)
+    // put setup code here
 }
 
 function draw() {
-  // put drawing code here
+    // put drawing code here
 }
